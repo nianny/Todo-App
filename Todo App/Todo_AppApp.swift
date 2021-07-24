9a453b16c6ec6fd6fadc5d9eb183d06c8ae88c7f
nianny <xiaonianhe@hotmail.com>
2021-07-24 10:35:05 +0800

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Xiao Nianhe on 24/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
